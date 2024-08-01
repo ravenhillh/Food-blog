78c92092be6c3c160b0470508c79d2b7a9a14e90
ravenhillh <ravenhillh@gmail.com>
2024-08-01 15:08:33 -0500

    added *path to server

--- server/index.js
+++ server/index.js
@@ -139,6 +139,13 @@ app.post("/post/react", async (req, res) => {
     // }
 });
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("build"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    });
+  }
+
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
