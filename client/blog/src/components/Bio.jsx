127c73a1c0c03d008bdd480c6f3c4acafa29b231
ravenhillh <ravenhillh@gmail.com>
2024-08-07 09:37:39 -0500

    fix file path 2

--- client/blog/src/components/Bio.jsx
+++ client/blog/src/components/Bio.jsx
@@ -31,7 +31,7 @@ const Bio = () => {
                 </div>
             </nav>
         <div>
-          <img src="client/blog/src/assets/IMG_5746.jpg" alt="Bio picture" className="bio-pic"/>
+          <img src=".././assets/IMG_5746.jpg" alt="Bio picture" className="bio-pic"/>
         </div>
         <h1 style={{ display: "flex", alignItems: "center", padding: '50px' }}>My Story</h1>
         <p>Born and raised in Reading, PA. Eventually made my way out to Seattle, WA, in 1988. Worked at numerous restaurants across the city, including Tulio Ristorante, Cinque Terra, The Edgewater, Copperleaf, and Harry's Fine Foods, just to name a few. Living in the Delridge neighborhood of West Seattle, with wife Stacy Councilman, raised four+ children and spent free time writing, cooking and cycling.</p>
