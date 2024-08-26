554fd6a574a191e7084cea477196137e6287b320
ravenhillh <ravenhillh@gmail.com>
2024-08-26 09:58:58 -0500

    edit bio text

--- client/blog/src/components/Bio.jsx
+++ client/blog/src/components/Bio.jsx
@@ -5,12 +5,13 @@ const Bio = () => {
 //   const [loggedIn, setLoggedIn] = useState(false);
 
   return (
-    <div>
-        <div className="bio-pic-container">
-          <img src="/IMG_5746.jpg" alt="Bio picture"/>
-        </div>
+    <div style={{background: 'yellow', padding: '20px'}}>
         <h1 style={{ display: "flex", alignItems: "center", padding: '50px' }}>My Story</h1>
-        <p>Born and raised in Reading, PA. Eventually made my way out to Seattle, WA, in 1988. Worked at numerous restaurants across the city, including Tulio Ristorante, Cinque Terra, The Edgewater, Copperleaf, and Harrys Fine Foods, just to name a few. Living in the Delridge neighborhood of West Seattle, with wife Stacy Councilman, raised four+ children and spent free time writing, cooking and cycling.</p>
+        <p>Born and raised in Reading, PA. I have worked in the kitchen since 1973, working in a camp kitchen as a teenager. My journey as a chef
+            has taken me from small town Pennsylvania to Philadelphia, to Seattle and eventually to the islands of Hawaii. I have recently worked
+            at such notable seattle restaurants as Cinque Terra, Cedarbrook Lodge and Harrys Fine Foods. I am a dedicated West Seattleite, living in
+            the Delridge neighborhood since 1999. In my free time I love to cook, write, ride bikes, and spend time with my wife and four children.
+        </p>
     </div>
   )
 }
