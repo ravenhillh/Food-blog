42f014579a59513c7e90c0bbbcd503d3a0095f1f
ravenhillh <ravenhillh@gmail.com>
2025-01-08 15:34:34 -0600

    change style

--- client/blog/src/components/Home.jsx
+++ client/blog/src/components/Home.jsx
@@ -1,8 +1,9 @@
 import { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { fetchAllPosts } from "../utils/util";
-import Bio from './Bio'
-import Photos from './Photos'
+// import Bio from './Bio'
+// import Photos from './Photos'
+
 
 const Home = () => {
     const [loggedIn, setLoggedIn] = useState(false);
@@ -24,26 +25,36 @@ const Home = () => {
 
     return (
         <div>
-            <nav className='navbar'>
-                <Link to='/' className='logo'>
-                    <h2>Jon Hill Recipes</h2>
-                </Link>
-                {/* <Link to='/bio' className="logo">Bio</Link> */}
-                <div style={{ display: "flex", alignItems: "center" }}>
-                    {loggedIn ? (
-                        <Link to='/post/new' className='newPostBtn'>
-                            New Post
-                        </Link>
-                    ) : (
-                        <Link to='/login' className='newPostBtn'>
-                            Log in
-                        </Link>
-                    )}
+            <div style={{ backgroundColor: 'black', height: '30px', width: '100%' }}></div>
+            <div className="landing-wrapper">
+                <nav style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }} className='navbar'>
+                    <Link to='/' className='logo'>
+                        <h2>Jon Hill</h2>
+                    </Link>
+                    <div style={{ display: 'flex', gap: '2rem', alignItems: 'center' }}>
+                        <Link to='/bio' className="logo"><h2>Bio</h2></Link>
+                        <Link to='/photos' className="logo"><h2>Gallery</h2></Link>
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                            {loggedIn ? (
+                                <Link to='/post/new' className='newPostBtn'>
+                                    New Post
+                                </Link>
+                            ) : (
+                                <Link to='/login' className='newPostBtn'>
+                                    Log in
+                                </Link>
+                            )}
+                        </div>
+                    </div>
+                </nav>
+                <div style={{ textAlign: 'center', marginTop: '60px', fontFamily: 'Playfair Display, serif' }}>
+                    <h2 style={{ fontSize: '2.5rem', marginBottom: '1rem' }}>Jon Hill Culinary Experience</h2>
+                    <h3 style={{ fontSize: '1.2rem', fontWeight: '300', color: "antiquewhite" }}>Explore the Recipes and Idiosyncratic Musings of Chef Jonathan Hill</h3>
                 </div>
-            </nav>
+            </div>
             <main className='main'>
-                < Photos/>
-                < Bio/>
+                {/* < Photos/>
+                < Bio/> */}
                 <h2 className='heading'>Latest Posts</h2>
                 <div className='posts_container'>
                     {posts?.map((post) => (
@@ -56,5 +67,4 @@ const Home = () => {
         </div>
     );
 };
-
 export default Home;
